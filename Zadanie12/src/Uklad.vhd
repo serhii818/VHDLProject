b630930bae53a3fccaca74633545f9f627bcc38e
Serhii Zasiadivko <serhiozsvk101@gmail.com>
2024-05-26 21:57:18 +0200

    that shit is finally working properly

--- Zadanie12/src/Uklad.vhd
+++ Zadanie12/src/Uklad.vhd
@@ -15,7 +15,14 @@ entity Uklad is
 		port_l1 : out std_logic_vector(2 downto 0);	 -- dla sledzenia wprowadzonych liczb
 		port_l2 : out std_logic_vector(2 downto 0);
 		port_l3 : out std_logic_vector(2 downto 0);
-		port_l4 : out std_logic_vector(2 downto 0)
+		port_l4 : out std_logic_vector(2 downto 0);
+		
+		out_stan : out std_logic_vector(2 downto 0); -- stan wprowadzenia sekwencji
+		przeprogramuwanie : out std_logic;
+		port_l1k : out std_logic_vector(2 downto 0);	 -- obecny pinkod
+		port_l2k : out std_logic_vector(2 downto 0);
+		port_l3k : out std_logic_vector(2 downto 0);
+		port_l4k : out std_logic_vector(2 downto 0)
 	);
 end Uklad;
 
@@ -23,19 +30,18 @@ end Uklad;
 
 architecture Uklad of Uklad is
 -- stan sekwencji liczb
-subtype stan_ukladu is integer range 0 to 4;
-type zaprogramuj is (false, true);
+subtype stan_ukladu is integer range 0 to 5;
+signal stan : stan_ukladu := 0;
 
 -- stan przeprogramuwania
-signal stan : stan_ukladu := 0;
-signal stan_zaprogramuwania : zaprogramuj := false; 
+signal stan_zaprogramuwania : std_logic := '0'; 
 
 -- pinkod zaprogramowany
 subtype kod is integer range 0 to 7;
-signal l1 : kod := 0;
-signal l2 : kod := 0;
-signal l3 : kod := 0;
-signal l4 : kod := 0;
+signal l1 : kod := 1;
+signal l2 : kod := 2;
+signal l3 : kod := 3;
+signal l4 : kod := 4;
 -- pinkod wprowadzony
 signal l1_in : kod;
 signal l2_in : kod;
@@ -53,7 +59,7 @@ begin
 			stan <= 0;
 			
 		elsif (change_pincode = '1') then -- wprowadzenie kodu
-			stan_zaprogramuwania <= true;
+			stan_zaprogramuwania <= '1';
 			stan <= 0;	
 			
 		elsif (delete = '1') then  -- remove one digit
@@ -63,37 +69,56 @@ begin
 			
 		elsif (accept = '1') then -- zapisuje wprowadzone dane do odpowiedniego sygnau wedlug stanu
 			case stan is
-				when 0 => l1_in <= to_integer(unsigned(data));
-				when 1 => l2_in <= to_integer(unsigned(data));
-				when 2 => l3_in <= to_integer(unsigned(data));
-				when 3 => l4_in <= to_integer(unsigned(data));
-				when others => 
-					sprawz_pinkod <= '1', '0' after 20 ns;
-					stan_zaprogramuwania <= false;
+				when 0 => 
+					l1_in <= to_integer(unsigned(data));
+					stan <= 1;
+				when 1 =>
+					l2_in <= to_integer(unsigned(data));
+					stan <= 2;			
+				when 2 => 
+					l3_in <= to_integer(unsigned(data));
+					stan <= 3;
+				when 3 => 
+					l4_in <= to_integer(unsigned(data));
+					sprawz_pinkod <= '1', '0' after 5ns;
 					stan <= 0;
-			end case;												 
+					stan_zaprogramuwania <= '0' after 10 ns;
+				when others => null; 
+			end case;
+
 		end if;
 	end process;
 	
-	comb:process(sprawz_pinkod)	 -- process kombonacyjny
+	process(sprawz_pinkod)
 	begin
-		if (stan_zaprogramuwania = true) then
-		   l1 <= l1_in;
-		   l2 <= l2_in;
-		   l3 <= l3_in;
-		   l4 <= l4_in;
-		else   
-			if ((l1 = l1_in) and (l2 = l2_in) and (l3 = l3_in) and (l4 = l4_in)) then
-				sig_open_lock <= '1', '0' after 20ns;
+		if (sprawz_pinkod = '1') then
+			if (stan_zaprogramuwania = '1') then
+			  	l1 <= l1_in;
+			   	l2 <= l2_in;
+			   	l3 <= l3_in;
+			   	l4 <= l4_in;
+
+			else   
+				if ((l1 = l1_in) and (l2 = l2_in) and (l3 = l3_in) and (l4 = l4_in)) then
+					sig_open_lock <= '1', '0' after 20ns;
+				end if;
 			end if;
 		end if;
-	end process;			  
+	end process;
+			  
 	
 	-- podlaczeine sygnalow
 	open_lock <= sig_open_lock;
+	out_stan <= std_logic_vector(to_unsigned(stan, 3));
+	przeprogramuwanie <= stan_zaprogramuwania;
+	
 	port_l1 <= std_logic_vector(to_unsigned(l1_in, 3));	
 	port_l2 <= std_logic_vector(to_unsigned(l2_in, 3));
 	port_l3 <= std_logic_vector(to_unsigned(l3_in, 3));
 	port_l4 <= std_logic_vector(to_unsigned(l4_in, 3));
+	port_l1k <= std_logic_vector(to_unsigned(l1, 3));	
+	port_l2k <= std_logic_vector(to_unsigned(l2, 3));
+	port_l3k <= std_logic_vector(to_unsigned(l3, 3));
+	port_l4k <= std_logic_vector(to_unsigned(l4, 3));
 
 end Uklad;
--- Zadanie12/src/Uklad_tb.vhd
+++ Zadanie12/src/Uklad_tb.vhd
@@ -1,12 +1,156 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity Uklad_tb is
 end Uklad_tb;
 
-
-
 architecture Uklad_tb of Uklad_tb is
+signal	data : 			std_logic_vector(2 downto 0);
+signal	accept : 		std_logic;
+signal	delete : 		std_logic;
+signal	change_pincode: std_logic; 
+signal	reset :  		std_logic;		   
+
+signal	open_lock : 	std_logic;	
+		
+signal	port_l1 :  		std_logic_vector(2 downto 0);	 
+signal	port_l2 :  		std_logic_vector(2 downto 0);
+signal	port_l3 :  		std_logic_vector(2 downto 0);
+signal	port_l4 :  		std_logic_vector(2 downto 0);
+
+signal	port_l1k :  	std_logic_vector(2 downto 0);	 
+signal	port_l2k :  	std_logic_vector(2 downto 0);
+signal	port_l3k :  	std_logic_vector(2 downto 0);
+signal	port_l4k :  	std_logic_vector(2 downto 0); 
+
+signal	out_stan :  	std_logic_vector(2 downto 0);  
+signal  przeprogramuwanie : std_logic;
 begin
 
+	utt : entity work.Uklad port map(
+		data => data,
+		accept => accept,		
+		delete => delete,
+		change_pincode => change_pincode,
+		reset => reset,
+		open_lock => open_lock,
+		port_l1 => port_l1,
+		port_l2 => port_l2,
+		port_l3 => port_l3,
+		port_l4 => port_l4,
+		port_l1k => port_l1k,
+		port_l2k => port_l2k,
+		port_l3k => port_l3k,
+		port_l4k => port_l4k,
+		out_stan => out_stan,
+		przeprogramuwanie => przeprogramuwanie
+	);
+
+	process
+	begin		
+		-- wprowadzenie pinkodu
+		data <= "001";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns;	
+		
+		data <= "010"; 	
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns;
+		
+		data <= "111";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns;
+		
+		-- usuniecie jednej liczby
+		delete <= '1';
+		wait for 5 ns;
+		delete <= '0';
+		wait for 20 ns;	
+		
+		data <= "011";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns;
+		
+		data <= "100";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 100 ns;
+		
+		-- przeprogramowanie
+		change_pincode <= '1';
+		wait for 5ns;
+		change_pincode <= '0';
+		wait for 20ns;
+		-- wprowadzenie nowego pinkodu
+		data <= "111";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns;	
+		
+		data <= "010"; 	
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns; 
+		
+		data <= "101";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns;
+		
+		data <= "001";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 100 ns;
+		
+		-- wprowadzmy stary pinkod
+		data <= "001";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns;	
+		
+		data <= "010"; 	
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns; 
+		
+		data <= "011";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns;
+		
+		data <= "100";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 100 ns; 
+		-- niedziala
+		
+		-- wprowadzmy nowy pinkod
+		data <= "111";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns;	
+		
+		data <= "010"; 	
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns; 
+		
+		data <= "101";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 20 ns;
+		
+		data <= "001";
+		wait for 5ns;
+		accept <= '1', '0' after 10 ns;
+		wait for 100 ns; 
+		-- dziala
+		
+	end process;
 
 
 end Uklad_tb;
